539402279eecce93c2f4305a41e48e07eaa661c7
malyriax <malyriax@gmail.com>
2026-02-16 20:01:23 +0100

    internal calc part is now working, visuals next

--- src/calc.java
+++ src/calc.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
 public class calc extends JFrame implements ActionListener {
 
 JTextField field;
@@ -150,79 +152,174 @@ public calc(){
 
 
 
-     // creates an array where each index is operator or operanda
-    public static String[] createUsableForm(String finalInput){
-        String[] arrayOfOperatorsAndOperands = new String[finalInput.length() * 2];
-        java.util.Arrays.fill(arrayOfOperatorsAndOperands, "");
-        int amountOfNumbers = 0; 
-
-        for(int i = 0; i < finalInput.length(); i++){
-            String currentSimbol = String.valueOf(finalInput.charAt(i));
-            if("0123456789.".contains(currentSimbol)){
-                arrayOfOperatorsAndOperands[amountOfNumbers] = arrayOfOperatorsAndOperands[amountOfNumbers] + currentSimbol;
-            }
-            else if("*+/-^".contains(currentSimbol)){
-                amountOfNumbers = amountOfNumbers + 1;
-                arrayOfOperatorsAndOperands[amountOfNumbers] = currentSimbol;
-                amountOfNumbers = amountOfNumbers + 1;
-            }
+     // creates an array where each index is operator or operanda  
+    public static ArrayList<String> createUsableForm(String finalInput) {
+    ArrayList<String> equation = new ArrayList<>();
+    String currentNumber = "";
+    String previousSimbol = "x";
+    for (int i = 0; i < finalInput.length(); i++) {
+        String currentSymbol = String.valueOf(finalInput.charAt(i));
+        if(i > 0){
+            previousSimbol = String.valueOf(finalInput.charAt(i - 1));
         }
-        return arrayOfOperatorsAndOperands;
+        if ("0123456789.".contains(currentSymbol)) {
+            currentNumber += currentSymbol;
+        } 
+        else if ("*+/^".contains(currentSymbol)) {
+            //if (!currentNumber.isEmpty()) {
+            equation.add(currentNumber);
+            currentNumber = ""; 
+            //}
+            equation.add(currentSymbol);
         }
-
-
-    public static String[] removeBraces(String[] input){
-        int amountOfOddValues = 1;
-        String[] brace = input;
-        for(int i = 0; i < brace.length; i++){
-            if(brace[i].equals("(") || brace[i].equals(")")){
-                for(int j = i; j < brace.length - 1; j++){
-                    brace[j] = brace[j + 1];
+        else if ("-".contains(currentSymbol)){
+            if("+-/*^(".contains(previousSimbol ) || i == 0){
+                if(currentNumber.equals("-")){
+                    currentNumber = "";
                 }
-            brace[brace.length - amountOfOddValues] = "";
-            amountOfOddValues++;
-            i--;
+                else currentNumber += "-";
+            }
+            else if("0123456789.".contains(previousSimbol)){
+                equation.add(currentNumber);
+            currentNumber = ""; 
+            equation.add(currentSymbol);
             }
         }
-        // step 2
-        int extraSpaceCounter = 0;
-        for(int i = 0; i < brace.length; i++){
-            if(brace[i].equals("")){
-                extraSpaceCounter++;
-            }
-        }
-        String braceNoExtraSpace[] = new String[brace.length - extraSpaceCounter];
-        int insertIndex = 0;
-        for(int i = 0; i < braceNoExtraSpace.length; i++){
-            if(!brace[i].equals("")){
-            braceNoExtraSpace[insertIndex] = brace[i];
-            insertIndex++;
-            }
-        }
-    return braceNoExtraSpace;
     }
+        if (!currentNumber.isEmpty()) {
+        equation.add(currentNumber);
+         }
+       //  for(int i = 0; i < equation.size(); i++){
+       //     if(equation.get(i).contains("--")){
+        //        equation.set(i, equation.get(i).replace("--", ""));
+        //    }
+      //   }
+    return equation;
+    }
+    //public static String[] removeBraces(String[] input){
+     //   int amountOfOddValues = 1;
+     //   String[] brace = input;
+     //   for(int i = 0; i < brace.length; i++){
+     //       if(brace[i].equals("(") || brace[i].equals(")")){
+     //           for(int j = i; j < brace.length - 1; j++){
+      //              brace[j] = brace[j + 1];
+      //          }
+      //      brace[brace.length - amountOfOddValues] = "";
+      //      amountOfOddValues++;
+      //      i--;
+      //      }
+      //  }
+        // step 2
+     //   int extraSpaceCounter = 0;
+     //   for(int i = 0; i < brace.length; i++){
+      //      if(brace[i].equals("")){
+       //         extraSpaceCounter++;
+      //      }
+      //  }
+   //     String braceNoExtraSpace[] = new String[brace.length - extraSpaceCounter];
+        //java.util.Arrays.fill(braceNoExtraSpace, "");
+   //     int insertIndex = 0;
+   //     for(int i = 0; i < braceNoExtraSpace.length; i++){
+   //         if(!brace[i].equals("")){
+    //        braceNoExtraSpace[insertIndex] = brace[i];
+    //        insertIndex++;
+    //        }
+   //     }
+  // return braceNoExtraSpace;
+   // }
+    // I just found out about ArrayList omfg
+    public static ArrayList<String> solvePower(ArrayList<String> equation){
+        int initialSizeOfList = equation.size();
+        
+        for(int i = initialSizeOfList - 1; i >= 0; i--){
+            if(equation.get(i).equals("^")){
+                double currentResult = Math.pow(Double.parseDouble(equation.get(i-1)), Double.parseDouble(equation.get(i+1)));
+                equation.set(i - 1,String.valueOf(currentResult));
+                equation.remove(i);
+                equation.remove(i);
+            }
+        }
+        return equation;
+    }
+    public static ArrayList<String> solveMultDiv(ArrayList<String> equation){
+        int initialSize = equation.size() - 1;
+        for(int i = initialSize; i >=0; i--){
+            if(equation.get(i).equals("*")){
+                 double currentResult = Double.parseDouble(equation.get(i-1)) * Double.parseDouble(equation.get(i+1));
+                 equation.set(i - 1, String.valueOf(currentResult));
+                equation.remove(i);
+                equation.remove(i);
+            }
+            else if(equation.get(i).equals("/")){
+                 double currentResult = Double.parseDouble(equation.get(i-1)) / Double.parseDouble(equation.get(i+1));
+                 equation.set(i - 1, String.valueOf(currentResult));
+                equation.remove(i);
+                equation.remove(i);
+            }
+        }
+        return equation;
+    }
+    public static String solvePlusMinus(ArrayList<String> equation){
 
+        double firstValue = 0;
+        // no need since negatives are now at the same index
+      //  if(equation.get(0).equals("-")){
+      //      equation.add(0, "0");
+      //  } 
+        firstValue = Double.parseDouble(equation.get(0));
+        int initialSize = equation.size() - 1;
+        for(int i = 0; i < equation.size() - 1; i++){
+            if(equation.get(i).equals("+")){
+               firstValue = firstValue +  Double.parseDouble(equation.get(i+1));
+            }
+            if(equation.get(i).equals("-")){
+               firstValue = firstValue -  Double.parseDouble(equation.get(i+1));
+            }
+        }
+        return String.valueOf(firstValue);
+    } 
+    // here you can easily add any operands
+    public static String solveTheBrace(ArrayList<String> preparedArray){
+        String result = solvePlusMinus(solveMultDiv(solvePower(preparedArray)));
+        return result;
+    }
+    public static String[] solveAllBraces(String[] preparedArray){
 
-
+        for(int i = 0; i < preparedArray.length; i++){
+        String currentString = preparedArray[i];
+        ArrayList<String> currentStringParced = createUsableForm(currentString);
+        String currentSolution = solveTheBrace(currentStringParced);
+        // String currentSolutionBracesFailSafe = "(" + currentSolution + ")";
+        for(int j = 0; j < preparedArray.length; j++){
+            preparedArray[j] = preparedArray[j].replace(currentString, currentSolution);
+        }
+        }
+        
+        return preparedArray;
+    }
+    
     public static void main(String[] args) throws Exception {
        // calc newcalc = new calc();
       //  newcalc.setVisible(true);
-     String test = "((((5+2)-(2-5)+4";
+     //String test = "((((5*2)-(2-5))+4";
+     String test = "5-(-(-5+2)*2^2 + 5)*(5+1)-1";
         validInput(test);
         if(reasonableInput == false){
             resultString = "Invalid input, check amount of braces";
             System.out.println(resultString);
         } else {
     String act1 = addBracesIfNeeded(test);
-    System.out.println("Fixed String: " + act1); 
+   // System.out.println("Fixed String: " + act1); 
     
     String[] act2 = parceByBraces(act1);
-    System.out.println("Braces found: " + Arrays.toString(act2)); 
-    
-    String[] act3 = removeBraces(createUsableForm(act2[2])); 
-    System.out.println("Cleared array: " + Arrays.toString(act3)); 
-        
-        
+  //  System.out.println("Braces found: " + Arrays.toString(act2));
+   
+    String[] act3 = solveAllBraces(act2);
+   // System.out.println(Arrays.toString(act3));
+
+    System.out.println(act3[act3.length - 1]);
+      // System.out.println(solveMultDiv(createUsableForm(("--3*2 + 5)")))) ;
+      // System.out.println(Double.parseDouble("-5"));
     }
 }
 }
